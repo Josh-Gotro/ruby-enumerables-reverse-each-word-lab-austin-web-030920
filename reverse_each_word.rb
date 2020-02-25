f4628e9a71d5d64542cb9b67c61bcf45e3a684fe
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-25 18:26:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,49 @@
 require 'pry' #binding.pry
 
 def reverse_each_word(string)
-new = string.split
-newnew= new.collect do |reverso|  
-  reverso.reverse 
-  end
-  return newnew.join(' ')
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def reverse_each_word(string)
+# new = string.split
+# newnew= new.collect do |reverso|  
+#   reverso.reverse 
+#   end
+#   return newnew.join(' ')
+# end
